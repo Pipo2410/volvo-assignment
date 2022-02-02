52b75a673df610617241a3359550fd1da451fbe3
Pipo2410 <37326909+Pipo2410@users.noreply.github.com>
2022-02-02 10:56:54 +0100

    idk

---
+++ .next/static/q8aMTLAlRIiUp65y5OIpl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/679-773c81942b5ae85664dc.js","static/css/13519da085a394cb8e69.css","static/chunks/pages/index-d5f088f08f5c5b24eb87.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
